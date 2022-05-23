47530fb09ebb098cce84c6b43cc236625d8e1f6f
Mark Dekker <m.m.dekker@uu.nl>
2022-05-23 16:05:43 +0200

    Initial (empty) files

---
+++ ClassVarianceDecomp.py
@@ -0,0 +1,57 @@
+# ======================================== #
+# Class to do the variance decomposition
+# ======================================== #
+
+# =========================================================== #
+# PREAMBULE
+# Put in packages that we need
+# =========================================================== #
+
+import numpy as np
+
+# =========================================================== #
+# PREAMBULE
+# Put in packages that we need
+# =========================================================== #
+
+class variancedecomp(object):
+
+    # =========================================================== #
+    # INITIALIZATION
+    # =========================================================== #
+
+    def __init__(self):
+        print("# ==================================== #")
+        print("# Initializing variancedecomp class    #")
+        print("# ==================================== #")
+
+    class variablelists(object):
+        def read_variable_lists(self):
+            3
+    
+    class metadata(object):
+        def read_data(self):
+            3
+        
+        def clean_data(self):
+            3
+    
+        def transform_to_xarray(self):
+            3
+    
+    class ipccdata(object):
+        def read_data(self):
+            3
+        
+        def clean_data(self):
+            3
+    
+        def transform_to_xarray(self):
+            3
+    
+    class decomposition(object):
+        def drawing(self):
+            3
+
+    def save(self):
+        3
